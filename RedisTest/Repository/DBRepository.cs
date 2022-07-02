d27c36bf205dea947009e3413941fe6e8a228d79
LunAhEric <luochen9913@gmail.com>
2022-07-02 15:12:24 +0800

    建立 Repository

---
+++ RedisTest/Repository/DBRepository.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RedisTest.Models.DBEntity;
+using RedisTest.Repository.Interface;
+using System.Linq;
+
+namespace RedisTest.Repository
+{
+    public class DBRepository : IDBRepository
+    {
+        private readonly NumberDBContext _context;
+
+        public DBRepository(NumberDBContext context)
+        {
+            _context = context;
+        }
+        public NumberDBContext Context { get { return _context; } }
+
+        public void Create<T>(T entity) where T : class
+        {
+            _context.Entry(entity).State = EntityState.Added;
+        }
+
+        public void Update<T>(T entity) where T : class
+        {
+            _context.Entry(entity).State = EntityState.Modified;
+        }
+
+        public void Delete<T>(T entity) where T : class
+        {
+            _context.Entry(entity).State = EntityState.Deleted;
+        }
+
+        public IQueryable<T> GetAll<T>() where T : class
+        {
+            return _context.Set<T>();
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+
+
+    }
+}
---
+++ RedisTest/Repository/Interface/IDBRepository.cs
@@ -0,0 +1,15 @@
+﻿using RedisTest.Models.DBEntity;
+using System.Linq;
+
+namespace RedisTest.Repository.Interface
+{
+    public interface IDBRepository
+    {
+        public NumberDBContext Context { get; }
+        public void Create<T>(T entity) where T : class;
+        public void Update<T>(T entity) where T : class;
+        public void Delete<T>(T entity) where T : class;
+        public IQueryable<T> GetAll<T>() where T : class;
+        public void Save();
+    }
+}
