41131c6007150e67ba43de054527487030b45aa9
LunAhEric <luochen9913@gmail.com>
2022-07-02 15:12:04 +0800

    建立 DBEntity

---
+++ RedisTest/Models/DBEntity/NumberDBContext.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace RedisTest.Models.DBEntity
+{
+    public partial class NumberDBContext : DbContext
+    {
+        public NumberDBContext() { }
+        public NumberDBContext(DbContextOptions<NumberDBContext> options) : base(options) { }
+
+        public virtual DbSet<Number> Numbers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("data source=(LocalDb)\\MSSQLLocalDB;initial catalog=NumberDB;integrated security=True;MultipleActiveResultSets=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<Number>(entity =>
+            {
+                entity.ToTable("Number");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ RedisTest/Models/Dto/NumberDto.cs
@@ -0,0 +1,8 @@
+﻿namespace RedisTest.Models.Dto
+{
+    public class NumberDto
+    {
+        public int Id { get; set; }
+        public int NumberSet { get; set; }
+    }
+}
